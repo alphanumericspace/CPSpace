e444a8375dd88344385c09d2fda9e3fd28768f53
Farhan Kurnia Pratama <farhankurniapratama.ua@gmail.com>
2023-12-28 14:21:04 +0700

    update index.cpp

--- index.cpp
+++ index.cpp
@@ -2,7 +2,17 @@
 #include <string>
 #include <vector>
 #include <iomanip>
+#include <algorithm>
 
 int main() {
-   std::cout<<"Test"<<std::endl;
+   std::vector<int> number {
+      12, 2, 20, 6
+   };
+   int min, max;
+   min = std::range::min(number);
+   max = std::range::max(number);
+   
+   std::cout<<"Array: "<<number<<std::endl;
+   std::cout<<"Smallest: "<<min<<std::endl;
+   std::cout<<"Greatest: "<<max<<std::endl;
 }
