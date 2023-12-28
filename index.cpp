c315e8b647adebcbc83618e91596a948820c2242
Farhan Kurnia Pratama <farhankurniapratama.ua@gmail.com>
2023-12-28 15:04:55 +0700

    update index.cpp

--- index.cpp
+++ index.cpp
@@ -3,12 +3,5 @@
 #include <vector>
 
 int main() {
-   std::vector<char> inits = {
-      'N', 'M', 'P', 'S'
-   };
-   for(char& init : inits) {
-      if(init=='M') continue;
-      if(init==inits.end()) std::cout<<'\n';
-      std::cout<<init<<' ';
-   }
+   
 }
