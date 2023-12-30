1094de3e6912090b363c4cf09ae81df29991e822
Alphanumeric <alphanumericspace@gmail.com>
2023-12-30 21:24:22 +0700

    update unit_converter/main.cpp

--- unit_converter/main.cpp
+++ unit_converter/main.cpp
@@ -6,9 +6,9 @@
 
 int main() {
 	std::vector<std::string> list_menu {
-      "\nUnit Converter\n",
-      "\nWelcome to SPace Project, this is a tool to convert one unit to another based on international standards.\n",
-      "\nEnter the number according to the options provided below.\n\n",
+      "\nUnit Converter\n\n",
+      "Welcome to SPace Project, this is a tool to convert one unit to another based on international standards.\n\n",
+      "Enter the number according to the options provided below.\n\n",
 		"[1] Unit Converter\n",
 		"[2] Information\n",
 		"[3] See this project on GitHub.\n",
