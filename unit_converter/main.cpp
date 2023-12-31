e9f4d60054047af79e330b21e31df4e98e710c5f
Alphanumeric <alphanumericspace@gmail.com>
2023-12-31 21:26:46 +0700

    update unit_converter/main.cpp

--- unit_converter/main.cpp
+++ unit_converter/main.cpp
@@ -31,8 +31,8 @@ void unit_option(void) {
    std::cout<<"Select one of the unit conversion operations that you want: \n\n";
    for(std::string unit : unit_opt) {
       std::cout<<unit<<std::endl;
-      std::cout<<"\n";
    }
+   std::cout<<"\n";
 }
 
 int main() {
