0e3a9ed9c725fd540de9af3080b530ca016def0a
Alphanumeric <alphanumericspace@gmail.com>
2023-12-31 23:19:44 +0700

    update unit_converter/main.cpp

--- unit_converter/main.cpp
+++ unit_converter/main.cpp
@@ -11,16 +11,15 @@ void drawline(void) {
    newline();
    for(int i = 0; i < 40; ++i) {
       std::cout<<"-";
-   }
-   newline();
+   } newline();
 }
 void main_menu(void) {
    std::cout<<"|> Main Menu\n\n";
    std::vector<std::string> list_menu {
-		"[1] Unit Converter",
-		"[2] Information",
-		"[3] View the repository on GitHub",
-		"[0] Exit"
+		"[1](unc) Unit Converter",
+		"[2](inf) Information",
+		"[3](git) View the repository on GitHub",
+		"[0](ext) Exit"
 	};
 	for(std::string list : list_menu) {
 		std::cout<<list<<std::endl;
@@ -29,14 +28,14 @@ void main_menu(void) {
 }
 void unit_converter(void) {
    std::cout<<"|> Unit Converter\n\n";
-   std::vector<std::string> unit_opt {
-      "[1] Length", "[2] Area",
-      "[3] Volume", "[4] Weight",
-      "[5] Temperature", "[6] Speed",
-      "[7] Pressure", "[8] Power",
-      "[0] Abort"
+   std::vector<std::string> unit_option {
+      "[1](len) Length", "[2](are) Area",
+      "[3](vol) Volume", "[4](wgt) Weight",
+      "[5](tmp) Temperature", "[6](spd) Speed",
+      "[7](pre) Pressure", "[8](pow) Power",
+      "[0](abr) Abort"
    };
-   for(std::string unit : unit_opt) {
+   for(std::string unit : unit_option) {
       std::cout<<unit<<std::endl;
    }
    newline();
@@ -48,60 +47,60 @@ int main() {
    newline();
    main_menu();
 	std::string input;
-	while(!(input == "0")) {
+	while(!(input == "0" or input == "ext")) {
 		std::cout<<"Enter the option: ";
 		std::cin>>input;
 		drawline();
 		newline();
 		
-		if(input == "1") {
+		if(input == "1" or input == "unc") {
 		   unit_converter();
 		   
-		   std::string unit_opt;
-		   while(!(unit_opt == "0")) {
+		   std::string unit_option;
+		   while(!(unit_option == "0" or unit_option == "abr")) {
 		      std::cout<<"Enter the option: ";
-		      std::cin>>unit_opt;
+		      std::cin>>unit_option;
 		      newline();
-		      if(unit_opt == "1") {
+		      if(unit_option == "1" or unit_option == "len") {
 		         std::cout<<"Done\n\n";
 		      }
-		      else if(unit_opt == "2") {
+		      else if(unit_option == "2" or unit_option == "are") {
 		         std::cout<<"Done\n\n";
 		      }
-		      else if(unit_opt == "3") {
+		      else if(unit_option == "3" or unit_option == "vol") {
 		         std::cout<<"Done\n\n";
 		      }
-		      else if(unit_opt == "4") {
+		      else if(unit_option == "4" or unit_option == "wgt") {
 		         std::cout<<"Done\n\n";
 		      }
-		      else if(unit_opt == "5") {
+		      else if(unit_option == "5" or unit_option == "tmp") {
 		         std::cout<<"Done\n\n";
 		      }
-		      else if(unit_opt == "6") {
+		      else if(unit_option == "6" or unit_option == "spd") {
 		         std::cout<<"Done\n\n";
 		      }
-		      else if(unit_opt == "7") {
+		      else if(unit_option == "7" or unit_option == "pre") {
 		         std::cout<<"Done\n\n";
 		      }
-		      else if(unit_opt == "8") {
+		      else if(unit_option == "8" or unit_option == "pow") {
 		         std::cout<<"Done\n\n";
 		      }
-		      else if(unit_opt == "0") {
+		      else if(unit_option == "0" or unit_option == "abr") {
 		         std::cout<<"<| Operation aborted...\n";
 		         drawline();
 		         newline();
 		      }
-		      else std::cout<<"'"<<unit_opt<<"' not in option\n\n";
+		      else std::cout<<"'"<<unit_option<<"' not in option\n\n";
 		   }
 		   main_menu();
 		}
-		else if(input == "2") {
+		else if(input == "2" or input == "inf") {
 		   std::cout<<"Done\n\n";
 		}
-		else if(input == "3") {
+		else if(input == "3" or input == "git") {
 		   std::cout<<"Done\n\n";
 		}
-		else if(input == "0") {
+		else if(input == "0" or input == "ext") {
 		   std::cout<<"<| Exit the program...\n\n";
 		}
 		else std::cout<<"'"<<input<<"' not in option\n\n";
