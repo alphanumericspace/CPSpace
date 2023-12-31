e419e82a77a78d9fe5550964675957b732c8b19c
Alphanumeric <alphanumericspace@gmail.com>
2023-12-31 23:35:30 +0700

    update unit_converter/main.cpp

--- unit_converter/main.cpp
+++ unit_converter/main.cpp
@@ -1,9 +1,7 @@
 // Unit Converter
-
 #include <iostream>
 #include <vector>
 #include <string>
-
 void newline(void) {
    std::cout<<std::endl;
 }
@@ -11,8 +9,7 @@ void drawline(void) {
    newline();
    for(int i = 0; i < 40; ++i) {
       std::cout<<"-";
-   }
-   newline();
+   } newline();
 }
 void main_menu(void) {
    std::cout<<"|> Main Menu\n\n";
@@ -23,8 +20,7 @@ void main_menu(void) {
 	};
 	for(std::string list : list_menu) {
 		std::cout<<list<<std::endl;
-	}
-	newline();
+	} newline();
 }
 void unit_converter(void) {
    std::cout<<"|> Unit Converter\n\n";
@@ -37,10 +33,8 @@ void unit_converter(void) {
    };
    for(std::string unit : unit_option) {
       std::cout<<unit<<std::endl;
-   }
-   newline();
+   } newline();
 }
-
 int main() {
    std::cout<<"\nCPSpace Project - Unit Converter\n\nWelcome to CPSPace Project, this is a tool to convert one unit to another based on international standards.\n";
    drawline();
@@ -52,55 +46,39 @@ int main() {
 		std::cin>>input;
 		drawline();
 		newline();
-		
 		if(input == "1" or input == "unc") {
-		   unit_converter();
-		   
-		   std::string unit_option;
-		   while(!(unit_option == "0" or unit_option == "abr")) {
-		      std::cout<<"Enter the option: ";
-		      std::cin>>unit_option;
-		      newline();
-		      if(unit_option == "1" or unit_option == "len") {
-		         std::cout<<"Done\n\n";
-		      }
-		      else if(unit_option == "2" or unit_option == "are") {
-		         std::cout<<"Done\n\n";
-		      }
-		      else if(unit_option == "3" or unit_option == "vol") {
-		         std::cout<<"Done\n\n";
-		      }
-		      else if(unit_option == "4" or unit_option == "wgt") {
-		         std::cout<<"Done\n\n";
-		      }
-		      else if(unit_option == "5" or unit_option == "tmp") {
-		         std::cout<<"Done\n\n";
-		      }
-		      else if(unit_option == "6" or unit_option == "spd") {
-		         std::cout<<"Done\n\n";
-		      }
-		      else if(unit_option == "7" or unit_option == "pre") {
-		         std::cout<<"Done\n\n";
-		      }
-		      else if(unit_option == "8" or unit_option == "pow") {
-		         std::cout<<"Done\n\n";
-		      }
-		      else if(unit_option == "0" or unit_option == "abr") {
-		         std::cout<<"<| Operation aborted...\n";
-		         drawline();
-		         newline();
-		      }
-		      else std::cout<<"'"<<unit_option<<"' not in option\n\n";
-		   }
-		   main_menu();
-		}
-		else if(input == "2" or input == "inf") {
-		   std::cout<<"Done\n\n";
-		}
-		else if(input == "0" or input == "ext") {
-		   std::cout<<"<| Exit the program...\n\n";
-		}
-		else std::cout<<"'"<<input<<"' not in option\n\n";
-	}
-	return 0;
+         unit_converter();
+         std::string unit_option;
+         while(!(unit_option == "0" or unit_option == "abr")) {
+            std::cout<<"Enter the option: ";
+            std::cin>>unit_option;
+            newline();
+            if(unit_option == "1" or unit_option == "len") {
+               std::cout<<"Done\n\n";
+            } else if(unit_option == "2" or unit_option == "are") {
+               std::cout<<"Done\n\n";
+            } else if(unit_option == "3" or unit_option == "vol") {
+               std::cout<<"Done\n\n";
+            } else if(unit_option == "4" or unit_option == "wgt") {
+               std::cout<<"Done\n\n";
+            } else if(unit_option == "5" or unit_option == "tmp") {
+               std::cout<<"Done\n\n";
+            } else if(unit_option == "6" or unit_option == "spd") {
+               std::cout<<"Done\n\n";
+            } else if(unit_option == "7" or unit_option == "pre") {
+               std::cout<<"Done\n\n";
+            } else if(unit_option == "8" or unit_option == "pow") {
+               std::cout<<"Done\n\n";
+            } else if(unit_option == "0" or unit_option == "abr") {
+               std::cout<<"<| Operation aborted...\n";
+               drawline();
+               newline();
+            } else std::cout<<"'"<<unit_option<<"' not in option\n\n";
+         } main_menu();
+		} else if(input == "2" or input == "inf") {
+         std::cout<<"Done\n\n";
+		} else if(input == "0" or input == "ext") {
+         std::cout<<"<| Exit the program...\n\n";
+		} else std::cout<<"'"<<input<<"' not in option\n\n";
+	} return 0;
 }
