278b0825066a4ee0461ad187ebf29a21ce748598
Alphanumeric <alphanumericspace@gmail.com>
2024-01-02 15:29:18 +0700

    update unit_converter/main.cpp

--- unit_converter/main.cpp
+++ unit_converter/main.cpp
@@ -73,7 +73,7 @@ int main() {
    
 	std::string input;
 	while(!(input == "0")) {
-		std::cout<<"Enter the option: ";
+		std::cout<<"\nEnter the option: ";
 		std::cin>>input;
 		
 		if(input == "1") {
