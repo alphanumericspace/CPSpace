ba7a06b2891c71ea39ec908fc8774b8f81f41797
Alphanumeric <alphanumericspace@gmail.com>
2024-01-03 14:28:36 +0700

    update unit_converter/ctn.h

--- unit_converter/ctn.h
+++ unit_converter/ctn.h
@@ -105,7 +105,7 @@ void defn(std::string param, std::string input = "") {
       error.icon = "><";
       error.color = R;
       printHeader(error);
-      std::cout<<"Unavailable option\n\n";
+      std::cout<<"Unavailable option '"<<input<<"'\n\n";
    } else {
       std::cout<<"\n\n"<<B<<R<<"Parameter '"<<param<<"' Unavailable!"<<S<<"\n\n";
    }
