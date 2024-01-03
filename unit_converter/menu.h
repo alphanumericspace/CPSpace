b71ed02b3871b31e96399c2f6a647cd58fa96769
Alphanumeric <alphanumericspace@gmail.com>
2024-01-03 10:32:30 +0700

    update unit_converter/menu.h

--- unit_converter/menu.h
+++ unit_converter/menu.h
@@ -68,8 +68,8 @@ void length_units(void) {
    std::cout<<"\nEnter available options. Select one of the units that will be used as the conversion parameter for all units.\n\n";
 }
 
-std::string exitProgram() {
-   return std::cout<<BD<<C<<EX<<S<<"Exit Program\n\n";
+void exitProgram(void) {
+   std::cout<<BD<<C<<EX<<S<<"Exit Program\n\n";
 }
 
 #endif
