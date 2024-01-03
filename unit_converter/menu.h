3bc34279393f92eecfeb603b709c92d93fa5e429
Alphanumeric <alphanumericspace@gmail.com>
2024-01-03 10:41:12 +0700

    update unit_converter/menu.h

--- unit_converter/menu.h
+++ unit_converter/menu.h
@@ -68,6 +68,10 @@ void length_units(void) {
    std::cout<<"\nEnter available options. Select one of the units that will be used as the conversion parameter for all units.\n\n";
 }
 
+void input(void) {
+   std::cout<<BD<<B<<FI<<S;
+}
+
 void unavailableOption(std::string input) {
    std::cout<<BD<<R<<ER<<S<<"Unavailable Option '"<<input<<"'\n\n";
 }
