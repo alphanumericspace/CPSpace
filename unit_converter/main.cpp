d341e5caf8ef8b1ea36d9974287180fd993960fd
Alphanumeric <alphanumericspace@gmail.com>
2024-01-03 15:45:15 +0700

    update unit_converter/main.cpp

--- unit_converter/main.cpp
+++ unit_converter/main.cpp
@@ -10,7 +10,7 @@ int main() {
    interface::defn("main_header");
    interface::defn("main_menu");
 	std::string input;
-	while(!(input == "0")) {
+	while(input != "0") {
 	   interface::defn("input_header");
 		std::cin>>input;
 		std::cout<<"\n";
@@ -19,7 +19,7 @@ int main() {
          interface::defn("unit_converter");
          
          std::string unit;
-         while(!(unit == "-1")) {
+         while(unit != "-1") {
             interface::defn("input_header");
             std::cin>>unit;
             std::cout<<"\n";
@@ -28,7 +28,7 @@ int main() {
                interface::defn("length_units");
                
                std::string len;
-               while(!(len == "-1")) {
+               while(len != "-1") {
                   interface::defn("input_header");
                   std::cin>>len;
                   std::cout<<"\n";
@@ -45,9 +45,27 @@ int main() {
                      interface::defn("length_units");
                   }
                }
-            }
-            else if(unit == "2") {
-               std::cout<<"Done\n";
+            } else if(unit == "2") {
+               interface::defn("area_units");
+               
+               std::string are;
+               while(len != "-1") {
+                  interface::defn("input_header");
+                  std::cin>>are;
+                  std::cout<<"\n";
+                  
+                  if(len == "-1") {
+                     interface::defn("unit_converter");
+                  }
+                  else if(len == "0") {
+                     interface::defn("exit_header");
+                     return 0;
+                  }
+                  else {
+                     interface::defn("error_header", are);
+                     interface::defn("length_units");
+                  }
+               }
             }
             else if(unit == "3") {
                std::cout<<"Done\n";
