f1b0e487f7a2c2398dc1976f382b59269a278a7e
Alphanumeric <alphanumericspace@gmail.com>
2024-01-04 17:25:25 +0700

    update unit_converter/main.cpp

--- unit_converter/main.cpp
+++ unit_converter/main.cpp
@@ -6,61 +6,61 @@
 #include "interface.h"
 
 int main() {
-   interface::defn("main_header");
-   interface::defn("main_menu");
+   cli::defn("main_header");
+   cli::defn("main_menu");
 	std::string input;
 	while(input != "0") {
-	   interface::defn("input_prefix");
+	   cli::defn("input_prefix");
 		std::cin>>input;
-		interface::defn("newline");
+		cli::defn("newline");
 		
 		if(input == "1") {
-         interface::defn("unit_converter");
+         cli::defn("unit_converter");
          
          std::string unit;
          while(unit != "-1") {
-            interface::defn("input_prefix");
+            cli::defn("input_prefix");
             std::cin>>unit;
-            interface::defn("newline");
+            cli::defn("newline");
             
             if(unit == "1") {
-               interface::defn("length_units");
+               cli::defn("length_units");
                
                std::string len;
                while(len != "-1") {
-                  interface::defn("input_prefix");
+                  cli::defn("input_prefix");
                   std::cin>>len;
-                  interface::defn("newline");
+                  cli::defn("newline");
                   
                   if(len == "-1") {
-                     interface::defn("unit_converter");
+                     cli::defn("unit_converter");
                   }
                   else if(len == "0") {
-                     interface::defn("exit_prefix");
+                     cli::defn("exit_prefix");
                   }
                   else {
-                     interface::defn("unavailable_option_prefix", len);
-                     interface::defn("length_units");
+                     cli::defn("unavailable_option_prefix", len);
+                     cli::defn("length_units");
                   }
                }
             } else if(unit == "2") {
-               interface::defn("area_units");
+               cli::defn("area_units");
                
                std::string are;
                while(are != "-1") {
-                  interface::defn("input_prefix");
+                  cli::defn("input_prefix");
                   std::cin>>are;
-                  interface::defn("newline");
+                  cli::defn("newline");
                   
                   if(are == "-1") {
-                     interface::defn("unit_converter");
+                     cli::defn("unit_converter");
                   }
                   else if(are == "0") {
-                     interface::defn("exit_prefix");
+                     cli::defn("exit_prefix");
                   }
                   else {
-                     interface::defn("unavailable_option_prefix", are);
-                     interface::defn("length_units");
+                     cli::defn("unavailable_option_prefix", are);
+                     cli::defn("length_units");
                   }
                }
             }
@@ -86,26 +86,26 @@ int main() {
                std::cout<<"Done\n";
             }
             else if(unit == "-1") {
-               interface::defn("main_menu");
+               cli::defn("main_menu");
             }
             else if(unit == "0") {
-               interface::defn("exit_prefix");
+               cli::defn("exit_prefix");
             }
             else {
-               interface::defn("unavailable_option_prefix", unit);
-               interface::defn("unit_converter");
+               cli::defn("unavailable_option_prefix", unit);
+               cli::defn("unit_converter");
             }
          }
 		}
 		else if(input == "2") {
-         interface::defn("information");
+         cli::defn("information");
 		}
 		else if(input == "0") {
-         interface::defn("exit_prefix");
+         cli::defn("exit_prefix");
 		}
 		else {
-         interface::defn("unavailable_option_prefix", input);
-	      interface::defn("main_menu");
+         cli::defn("unavailable_option_prefix", input);
+	      cli::defn("main_menu");
 		}
 	}
 	return 0;
