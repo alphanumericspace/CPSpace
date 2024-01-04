e9cd16c9f6a04b3b6212eeedde77e0e3da3e61c1
Alphanumeric <alphanumericspace@gmail.com>
2024-01-04 21:34:56 +0700

    update unit_converter/main.cpp

--- unit_converter/main.cpp
+++ unit_converter/main.cpp
@@ -8,88 +8,9 @@
 int main() {
    cli::def("header");
    cli::def("main");
-	
-	std::string mainMenu;
-	do {
-	   cli::def("input");
-      std::cin>>mainMenu;
-      cli::def("ln");
-      int _mainMenu = std::stoi(mainMenu);
-	   switch(_mainMenu) {
-         case 1:
-            cli::def("cvtr");
-            int unitCvtr;
-            do {
-               cli::def("input");
-               std::cin>>unitCvtr;
-               cli::def("ln");
-               switch(unitCvtr) {
-                  case 1:
-                     cli::def("length");
-                     int lengthUnit;
-                     do {
-                        cli::def("input");
-                        std::cin>>lengthUnit;
-                        cli::def("ln");
-                        switch(lengthUnit) {
-                           case 99:
-                              cli::def("cvtr");
-                              break;
-                           case 0:
-                              cli::def("exit");
-                              break;
-                           default:
-                              cli::def("err_opt");
-                              break;
-                        }
-                     } while(lengthUnit != 0 || lengthUnit != 99);
-                     break;
-                  case 2:
-                     cli::def("area");
-                     int areaUnit;
-                     do {
-                        cli::def("input");
-                        std::cin>>areaUnit;
-                        cli::def("ln");
-                        switch(lengthUnit) {
-                           case 99:
-                              cli::def("cvtr");
-                              break;
-                           case 0:
-                              cli::def("exit");
-                              break;
-                           default:
-                              cli::def("err_opt");
-                              break;
-                        }
-                     } while(areaUnit != 0 || areaUnit != 99);
-                     break;
-                  case 99:
-                     cli::def("main");
-                     break;
-                  case 0:
-                     cli::def("exit");
-                  default:
-                     cli::def("err_opt");
-                     break;
-               }
-            } while(unitCvtr != 0 || unitCvtr != 99);
-            break;
-         case 2:
-            cli::def("info");
-            break;
-         case 0:
-            cli::def("exit");
-            break;
-         default:
-            cli::def("err_opt");
-            break;
-      }
-	} while(mainMenu != "0");
-	
-	return 0;
-	
-	/*while(input != "0") {
+   
+   std::string input;
+	while(input != "0") {
 	   cli::def("input");
 		std::cin>>input;
 		cli::def("ln");
@@ -97,25 +18,25 @@ int main() {
 		if(input == "1") {
          cli::def("cvtr");
          
-         std::string unit;
-         while(unit != "-1") {
+         std::string unitCvtr;
+         while(unitCvtr != "-1") {
             cli::def("input");
-            std::cin>>unit;
+            std::cin>>unitCvtr;
             cli::def("ln");
             
-            if(unit == "1") {
+            if(unitCvtr == "1") {
                cli::def("length");
                
-               std::string len;
-               while(len != "-1") {
+               std::string length;
+               while(length != "-1") {
                   cli::def("input");
-                  std::cin>>len;
+                  std::cin>>length;
                   cli::def("ln");
                   
-                  if(len == "-1") {
+                  if(length == "-1") {
                      cli::def("cvtr");
                   }
-                  else if(len == "0") {
+                  else if(length == "0") {
                      cli::def("exit");
                   }
                   else {
@@ -123,19 +44,19 @@ int main() {
                      cli::def("length");
                   }
                }
-            } else if(unit == "2") {
+            } else if(unitCvtr == "2") {
                cli::def("area");
                
-               std::string are;
-               while(are != "-1") {
+               std::string area;
+               while(area != "-1") {
                   cli::def("input");
-                  std::cin>>are;
+                  std::cin>>area;
                   cli::def("ln");
                   
-                  if(are == "-1") {
+                  if(area == "-1") {
                      cli::def("cvtr");
                   }
-                  else if(are == "0") {
+                  else if(area == "0") {
                      cli::def("exit");
                   }
                   else {
@@ -144,31 +65,31 @@ int main() {
                   }
                }
             }
-            else if(unit == "3") {
+            else if(unitCvtr == "3") {
                std::cout<<"Done\n";
             }
-            else if(unit == "4") {
+            else if(unitCvtr == "4") {
                std::cout<<"Done\n";
             }
-            else if(unit == "5") {
+            else if(unitCvtr == "5") {
                std::cout<<"Done\n";
             }
-            else if(unit == "6") {
+            else if(unitCvtr == "6") {
                std::cout<<"Done\n";
             }
-            else if(unit == "7") {
+            else if(unitCvtr == "7") {
                std::cout<<"Done\n";
             }
-            else if(unit == "8") {
+            else if(unitCvtr == "8") {
                std::cout<<"Done\n";
             }
-            else if(unit == "9") {
+            else if(unitCvtr == "9") {
                std::cout<<"Done\n";
             }
-            else if(unit == "-1") {
+            else if(unitCvtr == "-1") {
                cli::def("main");
             }
-            else if(unit == "0") {
+            else if(unitCvtr == "0") {
                cli::def("exit");
             }
             else {
@@ -188,5 +109,5 @@ int main() {
 	      cli::def("main");
 		}
 	}
-	return 0;*/
+	return 0;
 }
