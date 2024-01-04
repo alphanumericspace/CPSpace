81ebb22474456d0798230545b35670a4e1a86428
Alphanumeric <alphanumericspace@gmail.com>
2024-01-04 18:49:26 +0700

    update unit_converter/cli.h

--- unit_converter/cli.h
+++ unit_converter/cli.h
@@ -31,24 +31,24 @@ void defn(const std::string param) {
    };
    
    struct Menu {
-      std::string menu_name;
-      std::vector<std::string> menu_contents;
-      std::string menu_description;
+      std::string name;
+      std::vector<std::string> contents;
+      std::string description;
       void printMenu(void) {
          Prefix menu;
          menu.prefix(1);
-         if(!menu_name.empty()) {
-            std::cout<<menu_name<<"\n\n";
+         if(!name.empty()) {
+            std::cout<<name<<"\n\n";
          } else std::cout<<"Unknown\n\n";
          
-         if(menu_contents.size() > 1) {
-            for(std::string content : menu_contents) {
+         if(contents.size() > 1) {
+            for(std::string content : contents) {
                std::cout<<content<<"\n";
             }
-         } else std::cout<<menu_contents[0]<<"\n";
+         } else std::cout<<contents[0]<<"\n";
          
-         if(!menu_description.empty()) {
-            std::cout<<"\n"<<menu_description<<"\n\n";
+         if(!description.empty()) {
+            std::cout<<"\n"<<description<<"\n\n";
          } else std::cout<<"\n";
       }
    };
@@ -57,18 +57,18 @@ void defn(const std::string param) {
       std::cout<<BG<<" CPSPace Project "<<S<<"\n\nThis tool is designed to seamlessly convert\nvarious units and simplify your calculations.\n\n";
    } else if(param == "main_menu") {
       Menu main_menu;
-      main_menu.menu_name = "Main Menu";
-      main_menu.menu_contents = {
+      main_menu.name = "Main Menu";
+      main_menu.contents = {
          "(1) Unit Converter",
          "(2) Information",
          "(0) Exit Program"
       };
-      main_menu.menu_description = "Enter available options.";
+      main_menu.description = "Enter available options.";
       main_menu.printMenu();
    } else if(param == "unit_cvtr") {
       Menu unit_cvtr;
-      unit_cvtr.menu_name = "Unit Converter";
-      unit_cvtr.menu_contents = {
+      unit_cvtr.name = "Unit Converter";
+      unit_cvtr.contents = {
          "(1) Length       |  (2) Area",
          "(3) Volume       |  (4) Weight",
          "(5) Temperature  |  (6) Speed",
@@ -76,12 +76,12 @@ void defn(const std::string param) {
          "(9) Data Size    |  (-1) Previous",
          "(0) Exit Program"
       };
-      unit_cvtr.menu_description = "Enter available options.";
+      unit_cvtr.description = "Enter available options.";
       unit_cvtr.printMenu();
    } else if(param == "length_units") {
       Menu length_units;
-      length_units.menu_name = "Length";
-      length_units.menu_contents = {
+      length_units.name = "Length";
+      length_units.contents = {
          "(1) Kilometer(km)          |  (2) Hectometer(hm)",
          "(3) Decameter(dam)         |  (4) Meter(m)",
          "(5) Decimeter(dm)          |  (6) Centimeter(cm)",
@@ -94,12 +94,12 @@ void defn(const std::string param) {
          "(19) Lunar Distance(ld)    |  (-1) Previous",
          "(0) Exit Program"
       };
-      length_units.menu_description = "Enter available options. Select one of the units that will be used as the conversion parameter for all units.";
+      length_units.description = "Enter available options. Select one of the units that will be used as the conversion parameter for all units.";
       length_units.printMenu();
    } else if(param == "area_units") {
       Menu area_units;
-      area_units.menu_name = "Area";
-      area_units.menu_contents = {
+      area_units.name = "Area";
+      area_units.contents = {
          "(1) Sq. Kilometer(km2)   |   (2) Sq. Hectometer(hm2)",
          "(3) Sq. Decameter(dam2)  |   (4) Sq. Meter(m2)",
          "(5) Sq. Decimeter(dm2)   |   (6) Sq. Centimeter(cm2)",
@@ -110,12 +110,12 @@ void defn(const std::string param) {
          "(15) Sq. Rod(rd2)        |   (-1) Previous",
          "(0) Exit Program"
       };
-      area_units.menu_description = "Enter available options. Select one of the units that will be used as the conversion parameter for all units.";
+      area_units.description = "Enter available options. Select one of the units that will be used as the conversion parameter for all units.";
       area_units.printMenu();
    } else if(param == "volume_units") {
       Menu volume_units;
-      volume_units.menu_name = "Unit Converter";
-      volume_units.menu_contents = {
+      volume_units.name = "Unit Converter";
+      volume_units.contents = {
          "(1) Cb. Kilometer(m3)     |  (2) Cb. Hectometer(hm3)",
          "(3) Cb. Decameter(dam3)   |  (4) Cb. Meter(m3)",
          "(5) Cb. Decimeter(dm3)    |  (6) Cb. Centimeter(cm3)",
@@ -124,7 +124,7 @@ void defn(const std::string param) {
          "(9)     |  (-1) Previous",
          "(0) Exit Program"
       };
-      volume_units.menu_description = "Enter available options. Select one of the units that will be used as the conversion parameter for all units.";
+      volume_units.description = "Enter available options. Select one of the units that will be used as the conversion parameter for all units.";
       volume_units.printMenu();
    } else if(param == "information") {
       std::cout<<"Done\n\n";
