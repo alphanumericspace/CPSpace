46fec9256c854cdd75b257ef1d06321154c5a7a0
Alphanumeric <alphanumericspace@gmail.com>
2024-01-04 18:02:34 +0700

    update unit_converter/interface.h

--- unit_converter/interface.h
+++ unit_converter/interface.h
@@ -1,14 +1,14 @@
 #ifndef cli
 #define cli
-
-void defn(const std::string param, const std::string input = "") {
-   const std::string B ("\033[34m");// Blue
+const std::string B ("\033[34m");// Blue
    const std::string G ("\033[32m"); // Green
    const std::string C ("\033[36m"); // Cyan
    const std::string R ("\033[31m"); // Red
    const std::string S ("\033[0m"); // Reset
    const std::string BD ("\033[1m"); // Bold
    const std::string BG ("\033[48;5;15m\033[38;5;0m"); // Background white & teks black
+void defn(const std::string param, const std::string input = "") {
+   
    
    struct Prefix {
       void prefix(const int key) {
