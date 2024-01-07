b58512d88c0ec596253db77a602fd390c357f3f7
Alphanumeric <alphanumericspace@gmail.com>
2024-01-07 11:40:38 +0700

    update unit_converter/main.cpp

--- unit_converter/main.cpp
+++ unit_converter/main.cpp
@@ -3,122 +3,120 @@
 #include <iostream>
 #include "cli.h"
 
-using namespace cli;
-
 int main() {
    std::cout<<"CPSPace Project\n\n"
             <<"This tool is designed to seamlessly convert\nvarious units and simplify your calculations.\n\n"
             <<"GitHub: https://github.com/alphanumericspace/CPSpace/ \n\n";
    
-   def("main");
+   cli::def("main");
    
    std::string main;
    do {
-      def("in");
+      cli::def("in");
       std::cin>>main;
-      def("ln");
+      cli::def("ln");
       
       if(main == "1") {
-         def("lenght");
+         cli::def("lenght");
          std::string length;
          do {
-            def("in");
+            cli::def("in");
             std::cin>>length;
-            def("ln");
-            if(length == "0") def("exit");
-            else def("unav", length);
+            cli::def("ln");
+            if(length == "0") cli::def("exit");
+            else cli::def("unav", length);
          } while(length != "-1");
-         def("main");
+         cli::def("main");
       } else if(main == "2") {
-         def("area");
+         cli::def("area");
          std::string area;
          do {
-            def("in");
+            cli::def("in");
             std::cin>>area;
-            def("ln");
-            if(area == "0") def("exit");
-            else def("unav", area);
+            cli::def("ln");
+            if(area == "0") cli::def("exit");
+            else cli::def("unav", area);
          } while(area != "-1");
-         def("main");
+         cli::def("main");
       } else if(main == "3") {
-         def("volume");
+         cli::def("volume");
          std::string volume;
          do {
-            def("in");
+            cli::def("in");
             std::cin>>volume;
-            def("ln");
-            if(volume == "0") def("exit");
-            else def("unav", volume);
+            cli::def("ln");
+            if(volume == "0") cli::def("exit");
+            else cli::def("unav", volume);
          } while(volume != "-1");
-         def("main");
+         cli::def("main");
       } else if(main == "4") {
-         def("weight");
+         cli::def("weight");
          std::string weight;
          do {
-            def("in");
+            cli::def("in");
             std::cin>>weight;
-            def("ln");
-            if(weight == "0") def("exit");
-            else def("unav", weight);
+            cli::def("ln");
+            if(weight == "0") cli::def("exit");
+            else cli::def("unav", weight);
          } while(weight != "-1");
-         def("main");
+         cli::def("main");
       } else if(main == "5") {
-         def("temp");
+         cli::def("temp");
          std::string temp;
          do {
-            def("in");
+            cli::def("in");
             std::cin>>temp;
-            def("ln");
-            if(temp == "0") def("exit");
-            else def("unav", temp);
+            cli::def("ln");
+            if(temp == "0") cli::def("exit");
+            else cli::def("unav", temp);
          } while(temp != "-1");
-         def("main");
+         cli::def("main");
       } else if(main == "6") {
-         def("speed");
+         cli::def("speed");
          std::string speed;
          do {
-            def("in");
+            cli::def("in");
             std::cin>>speed;
-            def("ln");
-            if(speed == "0") def("exit");
-            else def("unav", speed);
+            cli::def("ln");
+            if(speed == "0") cli::def("exit");
+            else cli::def("unav", speed);
          } while(speed != "-1");
-         def("main");
+         cli::def("main");
       } else if(main == "7") {
-         def("press");
+         cli::def("press");
          std::string press;
          do {
-            def("in");
+            cli::def("in");
             std::cin>>press;
-            def("ln");
-            if(press == "0") def("exit");
-            else def("unav", press);
+            cli::def("ln");
+            if(press == "0") cli::def("exit");
+            else cli::def("unav", press);
          } while(press != "-1");
-         def("main");
+         cli::def("main");
       } else if(main == "8") {
-         def("power");
+         cli::def("power");
          std::string power;
          do {
-            def("in");
+            cli::def("in");
             std::cin>>power;
-            def("ln");
-            if(power == "0") def("exit");
-            else def("unav", power);
+            cli::def("ln");
+            if(power == "0") cli::def("exit");
+            else cli::def("unav", power);
          } while(power != "-1");
-         def("main");
+         cli::def("main");
       } else if(main == "9") {
-         def("data");
+         cli::def("data");
          std::string data;
          do {
-            def("in");
+            cli::def("in");
             std::cin>>data;
-            def("ln");
-            if(data == "0") def("exit");
-            else def("unav", data);
+            cli::def("ln");
+            if(data == "0") cli::def("exit");
+            else cli::def("unav", data);
          } while(data != "-1");
-         def("main");
-      } else if(main == "0") def("exit");
-      else def("unav", main);
+         cli::def("main");
+      } else if(main == "0") cli::def("exit");
+      else cli::def("unav", main);
       
    } while(main != "0");
    
