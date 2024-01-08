0b2b2e9c6082d2857141350a6640d0928a782e47
Alphanumeric Space <alphanumericspace@gmail.com>
2024-01-08 15:53:23 +0700

    update unit_converter/main.cpp

--- unit_converter/main.cpp
+++ unit_converter/main.cpp
@@ -6,6 +6,7 @@
 int main() {
    std::cout<<"\033[1mAlphanumeric Space Project @CPSpace\033[0m\n\n"
             <<"This tool is designed to seamlessly convert\nvarious units and simplify your calculations.\n\n"
+            <<"Type: \033[1m'00'\033[0m or \033[1m'cvtr --help'\033[0m or \033[1m'cvtr -h'\033[0m for help."
             <<"\033[1mGitHub @https://github.com/alphanumericspace/CPSpace/\033[0m\n\n";
    
    cli::def("home");
