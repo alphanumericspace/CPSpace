51ca576244a604dfccf136c0529d087251f8b2db
Alphanumeric Space <alphanumericspace@gmail.com>
2024-01-08 16:04:53 +0700

    update unit_converter/cli.h

--- unit_converter/cli.h
+++ unit_converter/cli.h
@@ -9,13 +9,13 @@ void def(std::string key, std::string opt="") {
       
       void print(bool isWithDescription) {
          if(isWithDescription == true) {
-            std::cout<<"\033[1m@"<<head<<"\033[0m\n\n";
+            std::cout<<"\033[1m<< "<<head<<"\033[0m\n\n";
             for(std::string list : lists) {
                std::cout<<list<<"\n";
             }
             std::cout<<"\n"<<desc<<"\n\n";
          } else {
-            std::cout<<"\033[1m@"<<head<<"\033[0m\n\n";
+            std::cout<<"\033[1m<< "<<head<<"\033[0m\n\n";
             for(std::string list : lists) {
                std::cout<<list<<"\n";
             }
@@ -153,7 +153,7 @@ void def(std::string key, std::string opt="") {
    else if(key == "ln") std::cout<<"\n";
    else if(key == "unav") std::cout<<"\033[91m\033[1m!! Unavailable option: '"<<opt<<"'\033[0m\n\n";
    else if(key == "exit") {
-      std::cout<<"\033[91m\033[1m<< Exit Program...\033[0m\n\n";
+      std::cout<<"\033[91m\033[1m<> Exit Program...\033[0m\n\n";
       exit(0);
    }
    else std::cout<<"\033[91m\033[1m!! Invlaid key '"<<key<<"'\033[0m\n\n";
