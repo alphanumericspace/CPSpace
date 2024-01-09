749e8f9a4ba860522a26920e51fb41cbe0f7c5df
Alphanumeric Space <alphanumericspace@gmail.com>
2024-01-09 21:10:18 +0700

    update unit_converter/interface/cli.h

--- unit_converter/interface/cli.h
+++ unit_converter/interface/cli.h
@@ -203,7 +203,7 @@ void def(std::string key, std::string opt="") {
       List info;
       info.head = "A.S.P. @CPSpace - Unit Converter";
       info.desc = "This project is under development, check changelog for\n  more details or check this project at GitHub.";
-      info.Print(false);
+      info.Print(true);
    }
    else if(key == "docs") {
       
